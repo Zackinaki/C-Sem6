68b113befe47750426e052df710831cbf72cad07
IrinchinovZh <exp.bur@gmail.com>
2022-12-11 13:19:00 +0800

    add

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+// Генерация случайных дробных чисел
+//                                        * (end - begin) + begin
+double number = new Random().NextDouble() * (50 - 10) + 10; // [0, 1]
+// [begin; end]
+Console.WriteLine(number);
+
+
+// Console.Clear();
+// // Генерация случайных дробных чисел
+// //                                        * (end - begin) + begin
+// double number = Math.Round(new Random().NextDouble() * (50 - 10) + 10, 2); // [0, 1]
+// // [begin; end]
+// Console.WriteLine(number);
---
+++ 3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли 
+//существовать треугольник с сторонами такой длины.
+//Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.Clear();
+Console.WriteLine("Введите 3 числа: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+int n3 = Convert.ToInt32(Console.ReadLine());
+if (n1 < n2 + n3 && n2 < n1 + n3 && n3 < n1 + n2)
+    Console.WriteLine("Может");
+else
+    Console.WriteLine("Не может");
